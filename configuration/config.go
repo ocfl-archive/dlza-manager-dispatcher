69a8ab3da288f6d40d5762ecd3c52ae548898fdf
jarek <iaroslav.pavlov@unibas.ch>
2024-06-03 13:17:07 +0200

    Revert "Testing postgres with certificates"
    This reverts commit 369bbb9c7b26c441f4ea5c5791ee0fe66c818b5d.

--- configuration/config.go
+++ configuration/config.go
@@ -15,20 +15,13 @@ type Logging struct {
 	LogLevel string
 	LogFile  string
 }
-type GrpcAuth struct {
-	TlsCert string   `toml:"certificate"`    // TLS Certificate
-	TlsKey  string   `toml:"certificatekey"` // TLS Certificate Private Key
-	RootCA  []string `toml:"rootca"`         // additional root CA to trust
-
-}
 
 type Config struct {
-	Handler        Service  `yaml:"handler" toml:"Handler"`
-	StorageHandler Service  `yaml:"storage-handler" toml:"StorageHandler"`
-	Logging        Logging  `yaml:"logging" toml:"Logging"`
-	Dispatcher     Service  `yaml:"dispatcher" toml:"Dispatcher"`
-	CycleLength    int      `yaml:"cycle-length" toml:"CycleLength"`
-	GrpcConf       GrpcAuth `yaml:"grpc-conf" toml:"GrpcConf"`
+	Handler        Service `yaml:"handler" toml:"Handler"`
+	StorageHandler Service `yaml:"storage-handler" toml:"StorageHandler"`
+	Logging        Logging `yaml:"logging" toml:"Logging"`
+	Dispatcher     Service `yaml:"dispatcher" toml:"Dispatcher"`
+	CycleLength    int     `yaml:"cycle-length" toml:"CycleLength"`
 }
 
 // GetConfig creates a new config from a given environment
--- data/certs/embed.go
+++
@@ -1,8 +0,0 @@
-package certs
-
-import "embed"
-
-//go:embed ca-cert.pem
-//go:embed client-cert.pem
-//go:embed client-key.pem
-var CertFS embed.FS
